1d378217c5abb8b3e75f2e5fc3549234cd2895ad
anessheremeti <anessheremeti9@gmail.com>
2025-04-08 17:29:18 +0200

    admin dashboard

--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04e58c72ab9e6a24af2a0ee7af0aaa0100f143bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86508b5ab7d080920e2893c2c2c3e335285788ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
