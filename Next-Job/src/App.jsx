d0547ba925ac804b21a3b3c2d35c2b349a7094a8
EnsarZeqiri <ensarzeqiri15@gmail.com>
2025-04-09 15:17:07 +0200

    Withdrawal Front-End

--- Next-Job/src/App.jsx
+++ Next-Job/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Withdrawal from "../src/withdrawal/Withdrawal.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,8 +10,9 @@ function App() {
   return (
     <>
       <h1 className="text-6xl text-red-500">Hello</h1>
+      <Withdrawal></Withdrawal>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
