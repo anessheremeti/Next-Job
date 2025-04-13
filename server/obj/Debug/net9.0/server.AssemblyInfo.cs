62cd8c7e0840eb10657535b41315a89348e6c29a
BlendAjeti <blend.ajeti@hotmail.com>
2025-04-13 10:19:13 +0200

    commit

--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6440a73b6ce9805cb04954a899d3ac09b7b0606")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+690dfb2641fbe697ac14c3121f65ed3d1efa1c7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
