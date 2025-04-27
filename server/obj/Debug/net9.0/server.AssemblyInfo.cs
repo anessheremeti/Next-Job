1d3822e718605db6852c03e13cc93f31869ad94f
EnsarZeqiri <ensarzeqiri15@gmail.com>
2025-04-27 22:10:20 +0200

    nje ndryshim i vogel

--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+096a475c210a991c5d6375d3683ed1a0b21007f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb37c95226e9dfc2f8628a480c47abde0234b79f")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
